e42a96b41f76e0d691534f4485c1867de60752e6
Lucas Moura <lucasmoura.developer@hotmail.com>
2021-06-24 16:14:09 -0300

    fix: adicionado as variáveis (keys) no arquivo .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,5 @@
 import axios from 'axios';
-
-const TRAKT_API_KEY =
-  'd8e8e766c7067db59e5d627381550950b482fccd174ffb30a4f5c39875268ac0';
-
-const TMDB_API_KEY = 'f3deba27712e5a7969d5d993909a6123';
-const DEFAULT_LANGUAGE = 'pt-BR';
+import {TRAKT_API_KEY, TMDB_API_KEY} from '@env';
 
 export const TMDB_IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
@@ -22,10 +17,8 @@ export const apiTrakt = axios.create({
  * API usada apenas para buscar as imagens dos filmes.
  */
 export const apiTmdb = axios.create({
-  // https://api.themoviedb.org/3/movie/293660?api_key=f3deba27712e5a7969d5d993909a6123
   baseURL: 'https://api.themoviedb.org/3/',
   params: {
     api_key: TMDB_API_KEY,
-    // language: DEFAULT_LANGUAGE,
   },
 });
